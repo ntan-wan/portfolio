e93a1b07e87ac800882706ce9afaaeff0193c2a1
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-01-18 15:49:25 +0800

    Fix: fixed navbar issue
    Previously the content overlapped the navbar. Now it works perfectly.
    A reminder to myself, upload my personal projects and link up the
    necessary links such as contact infos and personal projects.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,27 @@ import React, {useEffect, useState} from 'react';
 import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai';
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
+import {useRouter} from 'next/router';
 
 const Navbar = () => {
-	const [nav, setNav] = useState(false);
-	const [shadow, setShadow] = useState(false);
+	const	[nav, setNav] = useState(false);
+	const	[shadow, setShadow] = useState(false);
+	const	[navBg, setNavBg] = useState('#ecf0f3');
+	const	[linkColor, setLinkColor] = useState('#1f2937');
+	const	router = useRouter();
+
+	useEffect(() => {
+		if (router.asPath === '/property')
+		{
+			setNavBg('transparent');
+			setLinkColor('#ecf0f3');
+		}
+		else
+		{
+			setNavBg('#ecf0f3');
+			setLinkColor('#1f2937');
+		}
+	}, [router]);
 
 	const handleNav = () => {
 		setNav(!nav);
@@ -24,13 +41,13 @@ const Navbar = () => {
 	},[]);
 
   return (
-	<div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+	<div style={{backgroundColor: `${navBg}`}} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
 		<div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
 			<Link href='/'>
 				<Image src="/../public/assets/navLogo.png" alt="/" width='125' height='50' />
 			</Link>
 			<div> 
-				<ul className='hidden md:flex'>
+				<ul style={{color: `${linkColor}`}} className='hidden md:flex'>
 					<Link href='/'>
 						<li className='ml-10 text-sm uppercase hover:border-b'>
 							Home
