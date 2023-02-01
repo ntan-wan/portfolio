44be094b3108fa71bba00f72f44c8730475e4f0d
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-02-01 19:15:50 +0800

    update

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,7 +14,8 @@ function Skills() {
 				<div className='p-6 shadow-xl rounde-xl hover:scale-105 ease-in duration-300'>
 					<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 						<div className='m-auto'>
-							<Image src='/../public/assets/skills/html.png' width='64' height='64' alt='/' />
+							{/* <Image src='/../public/assets/skills/html.png' width='64' height='64' alt='/' /> */}
+							<Image src={require('/../public/assets/skills/html.png')} width='64' height='64' alt='/' />
 						</div>
 						<div className='flex flex-col items-center justify-center'>
 							<h3>HTML</h3>
