941b5a9fe2457d371cae2bb779445989bd53f9ec
wang190 <89770062+wangwang190@users.noreply.github.com>
2024-04-02 07:53:01 +0800

    fixed icon typescript error

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,13 +1,12 @@
 import { icons } from 'lucide-react';
 
-// type LucideIcon
-type Props = {
-    name:string, 
+interface IProps  {
+    name: keyof typeof icons;
     color?:string,
     size?:number
 }
 
-export function Icon  ({ name, color, size = 14 } :Props) {
+export function Icon  ({ name, color, size = 14 } :IProps) {
   const LucideIcon = icons[name];
 
   return <LucideIcon color={color} size={size} />;
